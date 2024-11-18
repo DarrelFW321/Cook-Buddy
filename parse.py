d965285bbf5a14c12ae4ae249508a5aa6cafdba5
Darrel Feiro Wihandi <dfwihandi@uwaterloo.ca>
2024-11-17 20:49:14 -0500

    Update parse.py

--- parse.py
+++ parse.py
@@ -6,6 +6,26 @@ def parser(text):
     sentences = [sentence.strip() for sentence in sentences if sentence]
     return sentences
 
+# Parse type of instruction
+def parse_type(text):
+    sentences = parser(text)
+    if re.search(r'\bdone\b', sentences[0].lower()):
+        return "done"
+    
+    if re.search(r'\bthis is a recipe\b', sentences[0].lower()):
+        return "recipe"
+    
+    return None
+
+#Parse actual instruction
+def parse_instruction(text):
+    match = re.search(r'Instructions:\s*\[(.*?)\]', text)
+    if match:
+        instructions_text = match.group(1)
+        instructions = re.findall(r'"(.*?)"', instructions_text)
+
+    return instructions
+    
 # Handles fractional values
 def convert_to_decimal(value):
     if value is None: return 0.0
@@ -34,53 +54,55 @@ def parse_time(sentence):
         return total_time
     return None
 
-def checktime(sentences):
-    for sentence in sentences:
-        sentence = sentence.lower()
-        time = parse_time(sentence)
-        if time:
-            print(f"Checking sentence:{sentence}")
-            return time
-    return None
-
-def checktemp(text):
-    for sentence in text:
-        sentence.lower()
+def checktime(sentence):
+    sentence = sentence.lower()
+    time = parse_time(sentence)
+    if time:
         print(f"Checking sentence:{sentence}")
-        match = re.search(r'(\d+\.\d+)°C', sentence) or re.search(r'(\d+)°C', sentence) or re.search(r'(\d+\.\d+)F', sentence) or re.search(r'(\d+)F', sentence)
+        return time
+    return None
 
-        if match:
-            temperature = match.group(1)
-            print(f"Temperature detected: {temperature}°")
-            seconds = parse_time(sentence)
-            # TODO: Need to differentiate between Fahrenheit and Celsius
-            if seconds and seconds > 0:
-                monitorTemp(temperature, seconds)
-            else:
-                return temperature    
+def checktemp(sentence):
+    sentence.lower()
+    print(f"Checking sentence:{sentence}")
+    match = re.search(r'(\d+\.\d+)°C', sentence) or re.search(r'(\d+)°C', sentence) or re.search(r'(\d+\.\d+)F', sentence) or re.search(r'(\d+)F', sentence)
+
+    if match:
+        temperature = match.group(1)
+        print(f"Temperature detected: {temperature}°")
+        seconds = parse_time(sentence)
+        # TODO: Need to differentiate between Fahrenheit and Celsius
+        if seconds and seconds > 0:
+            monitorTemp(temperature, seconds)
+        else:
+            return temperature    
     return None
 
 
-def scale(text):
-    for sentence in text:
-        sentence.lower()
-        match_grams = re.search(r'(\d+/\d+|\d+\.\d+|\d+)\s*(gram|grams)', sentence)
-        match_kgs = re.search(r'(\d+/\d+|\d+\.\d+|\d+)\s*(kg|kgs)', sentence)
-        match_lb = re.search(r'(\d+/\d+|\d+\.\d+|\d+)\s*(lb|lbs)', sentence)
-        amount_in_grams = 0
+def scale(sentence):
+    sentence.lower()
+    match_grams = re.search(r'(\d+/\d+|\d+\.\d+|\d+)\s*(gram|grams)', sentence)
+    match_kgs = re.search(r'(\d+/\d+|\d+\.\d+|\d+)\s*(kg|kgs)', sentence)
+    match_lb = re.search(r'(\d+/\d+|\d+\.\d+|\d+)\s*(lb|lbs)', sentence)
+    amount_in_grams = 0
 
-        if match_grams:
-            amount = match_grams.group(1)
-            amount_in_grams = convert_to_decimal(amount)
+    if match_grams:
+        amount = match_grams.group(1)
+        amount_in_grams = convert_to_decimal(amount)
+        return amount_in_grams
         
-        elif match_kgs:
-            amount = match_kgs.group(1)
-            amount_in_kgs = convert_to_decimal(amount)
-            amount_in_grams = amount_in_kgs * 1000
-        
-        elif match_lb:
-            amount = match_lb.group(1)
-            amount_in_lb = convert_to_decimal(amount)
-            amount_in_grams = amount_in_lb * 453.592
+    
+    elif match_kgs:
+        amount = match_kgs.group(1)
+        amount_in_kgs = convert_to_decimal(amount)
+        amount_in_grams = amount_in_kgs * 1000
+        return amount_in_grams
+    
+    elif match_lb:
+        amount = match_lb.group(1)
+        amount_in_lb = convert_to_decimal(amount)   
+        amount_in_grams = amount_in_lb * 453.592
+        return amount_in_grams
+            
     return None
 
