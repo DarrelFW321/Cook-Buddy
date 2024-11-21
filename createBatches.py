0b46e5b4fa1a663ec604456dd9b8a13ca70b877e
m3di0cre3 <anandtandon8@gmail.com>
2024-11-20 22:09:26 -0500

    debugged createBatches to functionality

--- createBatches.py
+++ createBatches.py
@@ -7,30 +7,39 @@ import pandas as pd
 import openai
 import tiktoken
 import random
-from dotenv import load_dotenv
 import os
 import re
 import csv
 
 
-with open(r'RecipeNLG_dataset.csv', newline='') as csvFile:
-    csvReader = csv.reader(csvFile)
+csvReader = pd.read_csv(r'RecipeNLG_dataset.csv', nrows=51)
+
+recsPerRequest = 5      # customize
+requestsPerBatch = 2    # customize
+recsPerBatch = recsPerRequest * requestsPerBatch
+totalRequests = 50
+
+systemMessage = "For each recipe name reply with 1 word for each in format: Cuisine (e.g. French, Thai), dish type (e.g. pasta, curry), course (snack, entree). Separate recipe responses by line." # 46 tokens
+inputString = ""
+
+curBatchNum = 0
+currentFile = open(f"batchFiles/batch_{curBatchNum}.jsonl", 'w')
+curBatchNum += 1
+
+for i, recipe in csvReader.iterrows():
+    inputString += f"{recipe[1]}\n"
     
-    curBatchNum = -1
-    systemMessage = "For each recipe name reply with 1 word for each in format: Cuisine (e.g. French, Thai), dish type (e.g. pasta, curry), course (snack, entree). Separate recipe responses by line." # 46 tokens
-    inputString = ""
-    for request in csvReader:
-        if ((request[0]+1) % 50000 == 0):
-            curBatchNum += 1
-            os.makedirs("batchFiles", exist_ok=True)
-            fileName = f"batchFiles/batch_{curBatchNum}.jsonl"
-            currentFile = open(fileName, 'w')
-            
-        if ((request[0]+1) % 100 == 0):
-            inputString = inputString.rstrip()
-            requestDict = {"custom_id": f"request-{request[0]}", "method": "POST", "url": "/v1/chat/completions", "body": {"model": "gpt-4o-mini", "messages": [{"role": "system", "content": systemMessage}, {"role": "user", "content": inputString}]}}
-            currentFile.write(json.dumps(requestDict))
-            inputString = ""
-            
-        inputString += f"{request[1]}\n"
+    if ((i+1) % recsPerRequest == 0):
+        inputString = inputString.rstrip()
+        requestDict = {"custom_id": f"request-{int(((i+1)/recsPerRequest)-(requestsPerBatch-1))}", "method": "POST", "url": "/v1/chat/completions", "body": {"model": "gpt-4o-mini", "messages": [{"role": "system", "content": systemMessage}, {"role": "user", "content": inputString}]}}
+        currentFile.write(json.dumps(requestDict))
+        currentFile.write("\n")
+        inputString = "" 
+
+    if i >= totalRequests-1:
+        break
     
+    if ((i+1) % recsPerBatch == 0):
+        os.makedirs("batchFiles", exist_ok=True)
+        currentFile = open(f"batchFiles/batch_{curBatchNum}.jsonl", 'w')
+        curBatchNum += 1
