02c6e56c35bc82099c3fa368ace438ac5c2ec3e0
m3di0cre3 <anandtandon8@gmail.com>
2024-11-21 21:59:55 -0500

    ran 2 batches

--- submitBatches.py
+++ submitBatches.py
@@ -3,14 +3,15 @@ import dotenv
 import os
 
 # SET THE FOLLOWING FOR EVERY BATCH
-key1or2 = 1
-batchNum = 0
+key1or2 = 2
+batchNum = 1
 
-client = openai.OpenAI()
 
 dotenv.load_dotenv()
 api_key = os.getenv(f"API_KEY{key1or2}")
 openai.api_key = api_key
+
+client = openai.OpenAI(api_key=api_key)
 
 
 batch_file = client.files.create(
@@ -25,6 +26,7 @@ batch_job = client.batches.create(
 )
 
 batchIDFile = open(f"batchIDs", 'a')
-batchIDFile.write(f"batch_{batchNum}: {batch_file.id}")
+batchIDFile.write(f"batch_{batchNum}: {batch_file.id}\n")
+batchIDFile.close()
 
 print(batch_job)
