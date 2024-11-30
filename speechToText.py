36822717b8c6efe37c056e2582791a2e19b39aa7
m3di0cre3 <anandtandon8@gmail.com>
2024-11-29 21:44:24 -0500

    tts and stt finished

--- speechToText.py
+++ speechToText.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, jsonify
+from google.oauth2 import service_account
 from google.cloud import speech
 import io
 
 app = Flask(__name__)
 
 @app.route('/speech-to-text', methods=['POST'])
-def speech_to_text():
+# when a window is detected to have words (non-empty string), start input again
+# call on small windows, when one or two windows return empty string, then stop appending user input and submit input, restarting
+def speech_to_text():       
     if 'file' not in request.files:
         return jsonify({'error': 'No file part'}), 400
 
@@ -13,15 +16,9 @@ def speech_to_text():
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
 
-    audio_content = file.read()
-
-    client = speech.SpeechClient()
-    audio = speech.RecognitionAudio(content=audio_content)
-    config = speech.RecognitionConfig(
-        encoding=speech.RecognitionConfig.AudioEncoding.MP3,
-        sample_rate_hertz=16000,        # set sample rate
-        language_code='en-US'
-    )
+    audioContent = file.read()
+    
+    audio = speech.RecognitionAudio(content=audioContent)
 
     response = client.recognize(config=config, audio=audio)
 
@@ -30,6 +27,17 @@ def speech_to_text():
         transcript += result.alternatives[0].transcript
 
     return jsonify({'transcript': transcript})
+
+client_file = "serviceAccKey.json"
+credentials = service_account.Credentials.from_service_account_file(client_file)
+client = speech.SpeechClient(credentials=credentials)
+
+config = speech.RecognitionConfig(
+    encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+    sample_rate_hertz=44100,  #whatever sample rate is used
+    language_code='en-US',
+    model='default',
+)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
---
+++ textToSpeech.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+import io
+from google.oauth2 import service_account
+from google.cloud import texttospeech
+import os
+
+
+app = Flask(__name__)
+
+@app.route('text-to-speech', method=['POST'])
+def text_to_speech():
+    if 'file' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+
+    file = request.files['file']
+    if file.filename == '':
+        return jsonify({'error': 'No selected file'}), 400
+    
+    textBlock = file.read()
+    
+    synthesisInput = texttospeech.SynthesisInput(text=textBlock)
+    
+    audioOut = client.synthesize_speech(input=synthesisInput, voice=voice, audio_config=audio_config)
+    
+    os.makedirs(r"speechOuts", exist_ok=True)
+    with open(r"speechOuts/speechOutput.mp3", "wb") as outFile:
+        outFile.write(audioOut.audio_content)
+    
+    return
+
+client_file = "serviceAccKey.json"
+credentials = service_account.Credentials.from_service_account_file(client_file)
+client = texttospeech.TextToSpeechClient(credentials=credentials)
+
+voice = texttospeech.VoiceSelectionParams(
+    language_code='en-US',
+    name='en-US-Journey-F'  # studio al: 'en-US-Studio-O'
+)
+
+audio_config = texttospeech.AudioConfig(
+    audio_encoding=texttospeech.AudioEncoding.MP3,
+    effects_profile_id=['small-bluetooth-speaker-class-device'],      #medium-bluetooth-speaker-class-device if we use a bigger speaker
+    # not supported for journey:
+    #speaking_rate=1,
+    #pitch=1
+)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  # changes values
